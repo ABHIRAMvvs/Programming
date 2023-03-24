490cf2fe86795983348a9920b56a558e29d0aa20
ABHIRAMvvs <saiabhiram16.vadrevus@gmail.com>
2023-03-24 18:29:22 +0530

    ..

--- BST.cpp
+++ BST.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-using namespace std;
-int maxDifference(node *root, int target){
-    if(target> root->data){
-        root 
+int maxDifference(Node *root, int target){
+    int  maxDiffValue = INT_MIN;
+     if (root == NULL) {
+        return 0;
     }
+    if (root->data == target) {
+        int leftSum = maxDiff(root->left, target, maxDiffValue);
+        int rightSum = maxDiff(root->right, target, maxDiffValue);
+        if (leftSum == -1 || rightSum == -1) {
+            return -1;
+        }
+        maxDiffValue = max(maxDiffValue, leftSum + rightSum);
+        return 0;
+    }
+    int leftSum = maxDiff(root->left, target, maxDiffValue);
+    int rightSum = maxDiff(root->right, target, maxDiffValue);
+    if (leftSum == -1 && rightSum == -1) {
+        return -1;
+    }
+    if (leftSum == -1) {
+        return rightSum + root->data;
+    }
+    if (rightSum == -1) {
+        return leftSum + root->data;
+    }
+    int sum = leftSum + rightSum + root->data;
+    maxDiffValue = max(maxDiffValue, sum - leftSum - rightSum);
+    return sum;
 }
